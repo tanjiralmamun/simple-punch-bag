cb60b98c2e4c4f5edc209022e08d2a3893a15703
Tanjir Al Mamun <tanjir.almamun@outlook.com>
2021-06-19 15:12:56 +0600

    added functionality and finalize the app

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 new Vue({
   el: '#vue-app',
   data: {
-
+    health: 100,
+    ended: false
   },
-  method: {
-    
+  methods: {
+    punch: function(){
+      this.health -= 10;
+      if(this.health <= 0){
+        this.ended = true
+      }
+    },
+    restart: function(){
+      this.health = 100;
+      this.ended = false
+    }
   }
 });
